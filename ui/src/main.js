a2754f9c5b002c77b133d11b7965d6fe01d7649b
Jocelyn <duong.huan2122000@gmail.com>
2021-06-26 11:24:16 +0700

    feat: Tích hợp oruga UI

--- ui/src/main.js
+++ ui/src/main.js
@@ -2,8 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Oruga from '@oruga-ui/oruga'
+import '@oruga-ui/oruga/dist/oruga.css'
 
 Vue.config.productionTip = false
+
+Vue.use(Oruga);
 
 new Vue({
   router,
--- ui/src/views/About.vue
+++ ui/src/views/About.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <o-button @click="clickMe">Click Me</o-button>
   </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    clickMe() {
+      alert("click");
+    },
+  },
+};
+</script>
